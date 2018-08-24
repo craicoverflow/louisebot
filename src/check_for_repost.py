8c9feb0326cabeb723925184569ac4b802fae067
Enda Phelan <endaphelan@gmail.com>
2018-08-24 12:30:19 +0100

    Filter out duplicates that are newer than current submission

--- src/check_for_repost.py
+++ src/check_for_repost.py
@@ -8,6 +8,14 @@ import pdb
 bot_name = os.getenv("ENV_BOT")
 subreddit_name = os.getenv("ENV_SUBREDDIT")
 
+def filter_by_subreddit(seq, value):
+    for el in seq:
+        if el.subreddit == value: yield el
+
+def filter_newer_duplicates(seq, submission):
+    for el in seq:
+        if submission.created_utc >= el.created_utc: yield el
+
 if bot_name is None: # We need to know the name of the bot
     print('Please provide bot name.')
 elif subreddit_name is None: # We need to know the name of the subreddit to scan
@@ -26,29 +34,30 @@ else:
 
             subreddit = reddit.subreddit(subreddit_name)
 
-            for submission in subreddit.new(limit=1):
+            for submission in subreddit.new(limit=10):
+
                 if submission.id not in posts_replied_to and submission.is_self is False:
 
-                    # If we find at least 1 duplicate link of the current submission
-                    if len(list(submission.duplicates())) > 0:
+                    duplicate_list = list(filter_by_subreddit(submission.duplicates(), subreddit_name))
+                    duplicate_list = list(filter_newer_duplicates(duplicate_list, submission))
 
-                        duplicate_list = list(submission.duplicates())
+                    # If we find at least 1 duplicate link of the current submission
+                    if len(duplicate_list) > 0:
 
                         last_submission = duplicate_list[0] # get the most recent duplicate
-
-                        print("{} is replying to: {}".format(bot_name, submission.permalink))
+                        
+                        print("{} found a repost: {}".format(bot_name, submission.permalink.encode('utf-8').strip()))
 
                         # form the full URL of the duplicate so we can let the poster know
-                        post_url = 'https://reddit.com/r/{}/{}'.format(subreddit, last_submission.id)
+                        post_url = 'https://reddit.com' + last_submission.permalink
 
+                        print('\n')
                         # Reply to the current submission with a message showing them that this has already been posted.
                         submission.reply("This has been [submitted already](" + post_url + ") you lazy bastard.\n____________________________________________________________________________\n*This is an automated bot. Have feedback? Just send me a message or reply to this comment!*")
-                            
+                                
                         # Add the submission ID to the list of IDs
                         posts_replied_to.append(submission.id)
-                    else:
-                        print('No duplicates to report this time!')
-                        
+
     # Update the text file with the new IDs of what has been replied to
     with open("posts_replied_to.txt", "w") as f:
         for post_id in posts_replied_to:
